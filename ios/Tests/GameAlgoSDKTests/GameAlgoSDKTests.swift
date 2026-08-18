13569f3bca0c6b5f0a8c2aed09a2fc3220649dd7
sunyuhao <sunyuhao@sunyuhaos-MacBook-Pro.local>
2026-08-18 13:43:35 +0800

    test(ios): accept UTC local timestamps

--- ios/Tests/GameAlgoSDKTests/GameAlgoSDKTests.swift
+++ ios/Tests/GameAlgoSDKTests/GameAlgoSDKTests.swift
@@ -1005,7 +1005,7 @@ final class GameAlgoSDKTests: XCTestCase {
             return
         }
         XCTAssertNotNil(value.range(
-            of: #"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}$"#,
+            of: #"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}(?:Z|[+-]\d{2}:\d{2})$"#,
             options: .regularExpression
         ))
         guard let utcTimestamp else { return }
